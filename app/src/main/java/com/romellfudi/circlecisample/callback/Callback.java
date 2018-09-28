db408df344e60bf120d45a8a1a1d3dde01efc016
Freddyfudi <romllz489@gmail.com>
2018-09-27 20:29:50 -0500

    change package & add new unit test(failed)

---
+++ app/src/main/java/com/romellfudi/circlecisample/callback/Callback.java
@@ -0,0 +1,13 @@
+package com.romellfudi.circlecisample.callback;
+
+/**
+ * @version 1.0
+ * @autor Romell Domínguez
+ * @date 2/10/18
+ */
+
+public interface Callback {
+
+    void returnData(Data data);
+
+}
